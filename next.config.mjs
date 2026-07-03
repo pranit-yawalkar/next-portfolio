6deebfed9cd42f826022b185e331cc2be13d2122
pranit-yawalkar <pranityawalkar10@gmail.com>
2026-07-03 16:58:02 +0530

    redirect changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'pranityawalkar.vercel.app',
+          },
+        ],
+        destination: 'https://pranityawalkar.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
